4d6ddaaa16b8269d2a2c786eeb4e9c7648d7ad23
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 22:18:59 +0300

    c öğrenirken yaptığım bir çalışma

---
+++ tek:çift_adedi.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int i= 0;
+	int x=0;
+	while (i<25) {
+	
+	printf("sayiniz %d, -> %d \n",i++,(3*i)+8);
+	}
+	if(i%2==0)	{
+	
+	printf("cift sayi adedi %d \n ",x++ );
+	}
+
+	getch();
+	return 0;
+	
+}
