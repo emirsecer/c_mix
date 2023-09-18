b9dc8484aa64a09fd41b0311bf08c7010175f8d2
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 22:14:55 +0300

    c öğrenirken yaptığım bir çalışma

---
+++ matris2.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	int n;
+	printf("matris oluşturmak için bir n degeri giriniz ");
+	scanf("%d",&n);
+	
+	int matris[n][n];
+	int i,j;
+	
+	for (i=0;i<n;i++){
+		for(j=0;j<n;){
+			
+		}
+	scanf("%d",&matris[i][j]);
+	} 
+	for (i=0;i<n;i++){
+		for(j=0;j<n;){
+			
+		}
+	printf("%d",matris[i][j]);
+	printf("\n ");
+	} 
+	
+	
+}
