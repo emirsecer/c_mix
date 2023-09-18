4fc3de49239da4bad79aaef1908a062effb9a2ed
emirsecer <135528914+emirsecer@users.noreply.github.com>
2023-09-18 22:16:39 +0300

    c öğrenirken yaptığım bir çalışma

---
+++ matris3.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int x,y,i,j,n;
+	printf("a matrisin x ve y degerlerini giriniz:");
+	scanf("%d",&x);
+		printf("a matrisin x ve y degerlerini giriniz:");
+	scanf("%d",&y);
+	
+	
+	
+	int matris[i][j];
+	
+	
+	for (i=0;i<x;i++){
+
+	 for (j=0;j<y	printf("a matrisin x ve y değerlerini giriniz:");
+	scanf("%d",&x);
+	;j++){
+
+
+	scanf("%d",&matris[i][j] );
+		
+}   
+		}
+		for (i=0;i<x;i++){
+
+	for (j=0;j<n;j++){
+
+	printf("%d",matris[i][j]);
+	
+	}	
+	printf("\n");
+}   
+}
+		
